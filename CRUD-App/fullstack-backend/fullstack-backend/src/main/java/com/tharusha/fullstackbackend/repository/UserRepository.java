bc3207f206b53d316d160fcd12fc0173cd35a8d0
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-18 12:37:47 +0530

    create db classes

---
+++ CRUD-App/fullstack-backend/fullstack-backend/src/main/java/com/tharusha/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tharusha.fullstackbackend.repository;
+
+import com.tharusha.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+
+}
