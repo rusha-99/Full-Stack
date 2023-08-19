b50953039e44a6c9e6d57a24d865cd4f6728ea1f
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-19 20:20:20 +0530

    create navbar

---
+++ CRUD-App/fullstack-frontend/src/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg  bg-secondary text-white">
+        <div className="container-fluid x">
+          <a className="navbar-brand" href="#">
+            Full Stack App
+                  </a>
+                  
+                  <button className='btn btn-outline-light'>Add User</button>
+        
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
