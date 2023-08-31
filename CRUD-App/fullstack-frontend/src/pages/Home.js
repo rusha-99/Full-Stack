cb770d37cece151eb1929fce8561c99db142e951
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-31 23:19:05 +0530

    Update Home.js

--- CRUD-App/fullstack-frontend/src/pages/Home.js
+++ CRUD-App/fullstack-frontend/src/pages/Home.js
@@ -37,8 +37,8 @@ const Home = () => {
                 <td>{user.email}</td>
                 <td>
                   <button className='btn btn-primary mx-2'>View</button>
-                  <button className='bn btn-outline-primary mx-2'>Edit</button>
-                  <button className='btn btn-damage mx-2'>Delete</button>
+                  <button className='btn btn-outline-primary mx-2'>Edit</button>
+                  <button className='btn btn-danger mx-2'>Delete</button>
                 </td>
               </tr>
             ))}
