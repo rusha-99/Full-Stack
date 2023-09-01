c09e922837f382042e5f396edc988e60d6ab1ffc
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-01 20:55:45 +0530

    Update AddUser.js

--- CRUD-App/fullstack-frontend/src/users/AddUser.js
+++ CRUD-App/fullstack-frontend/src/users/AddUser.js
@@ -1,6 +1,10 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const AddUser = () => {
+
+   let navigate = useNavigate()
   const [user, setUser] = useState({
     name: "",
     username: "",
@@ -16,58 +20,67 @@ const AddUser = () => {
   
   }
 
+  const onSubmit =async (e) => {
+    e.preventDefault()
+    await axios.post('http://localhost:8080/user', user)
+    navigate("/")
+  }
+
   return (
     <div className="container mt-5">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Register User</h2>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter your name"
-              name="name"
-              value={name}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
+          <form onSubmit={onSubmit}>
+            {/* <form onSubmit={(e)=>onSubmit(e)}> */}
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="Username" className="form-label">
-              Username
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter your username"
-              name="username"
-              value={username}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your username"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="Email" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter your email"
-              name="email"
-              value={email}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <button type="submit" className="btn btn-outline-primary">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-outline-danger mx-2">
-            Cancel
-          </button>
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter your email"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <button type="submit" className="btn btn-outline-danger mx-2">
+              Cancel
+            </button>
+          </form>
         </div>
       </div>
     </div>
