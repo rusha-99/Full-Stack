9b090d01ae2e130c6893a56ba809f7fa80f03823
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-09-01 17:37:55 +0530

    setup routes

--- CRUD-App/fullstack-frontend/src/layout/Navbar.js
+++ CRUD-App/fullstack-frontend/src/layout/Navbar.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg  bg-secondary text-white">
         <div className="container-fluid x">
-          <a className="navbar-brand" href="#">
-            Full Stack App
-                  </a>
-                  
-                  <button className='btn btn-outline-light'>Add User</button>
-        
+          {/* <Link to="/"> */}
+            <a className="navbar-brand" href="/">
+              Full Stack App
+            </a>
+          {/* </Link> */}
+
+          <Link className="btn btn-outline-light" to="/adduser">
+            Add User
+          </Link>
         </div>
       </nav>
     </>
