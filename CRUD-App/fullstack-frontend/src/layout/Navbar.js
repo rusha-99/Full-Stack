26e07699a76c4a196c5fed68e61b7275702f7834
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-10-03 01:08:41 +0530

    Do minnor updates

--- CRUD-App/fullstack-frontend/src/layout/Navbar.js
+++ CRUD-App/fullstack-frontend/src/layout/Navbar.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg  bg-secondary text-white">
-        <div className="container-fluid x">
-          {/* <Link to="/"> */}
-            <a className="navbar-brand" href="/">
-              Full Stack App
-            </a>
-          {/* </Link> */}
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Job Link
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
           <Link className="btn btn-outline-light" to="/adduser">
             Add User
           </Link>
         </div>
       </nav>
-    </>
+    </div>
   )
 }
-
-export default Navbar
--- CRUD-App/fullstack-frontend/src/pages/Home.js
+++ CRUD-App/fullstack-frontend/src/pages/Home.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link, useParams } from 'react-router-dom'
 
-const Home = () => {
+export default function Home() {
   const [users, setUsers] = useState([])
-  
-  useEffect(() => { 
+
+  const { id } = useParams()
+
+  useEffect(() => {
     loadUsers()
   }, [])
-  
-  const loadUsers = async() => {
-    const result = await axios.get('http://localhost:8080/users');
+
+  const loadUsers = async () => {
+    const result = await axios.get('http://localhost:8080/users')
     setUsers(result.data)
+  }
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/user/${id}`)
+    loadUsers()
   }
 
   return (
@@ -19,10 +27,10 @@ const Home = () => {
         <table className="table border shadow">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">S.N</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
               <th scope="col">Action</th>
             </tr>
           </thead>
@@ -36,9 +44,24 @@ const Home = () => {
                 <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>
-                  <button className='btn btn-primary mx-2'>View</button>
-                  <button className='btn btn-outline-primary mx-2'>Edit</button>
-                  <button className='btn btn-danger mx-2'>Delete</button>
+                  <Link
+                    className="btn btn-primary mx-2"
+                    to={`/viewuser/${user.id}`}
+                  >
+                    View
+                  </Link>
+                  <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
@@ -48,5 +71,3 @@ const Home = () => {
     </div>
   )
 }
-
-export default Home
