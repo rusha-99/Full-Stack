df20ee95f7d32b39f63bbc7d978381dd2a2056d0
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-10-03 00:32:06 +0530

    Revert "update repository"
    This reverts commit 99c6719c605931322172f19c95bc79715cadcdbe.

--- CRUD-App/fullstack-backend/fullstack-backend/src/main/java/com/tharusha/fullstackbackend/exception/UserNotFoundAdvice.java
+++ CRUD-App/fullstack-backend/fullstack-backend/src/main/java/com/tharusha/fullstackbackend/exception/UserNotFoundAdvice.java
@@ -1,11 +1,14 @@
 package com.tharusha.fullstackbackend.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
-@ControllerAdvice
+
 public class UserNotFoundAdvice {
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
